3361866fbfdb5cffd5bc18f3df5217b9ae8cd6c1
lobox920 <86623018+lobox920@users.noreply.github.com>
2021-09-21 21:20:58 +0000

    Update TPTools.lua

--- TPTools.lua
+++ TPTools.lua
@@ -1,37 +1,10 @@
-local Player = game.Players.LocalPlayer
-
-local HopperBin = Instance.new('HopperBin', localPlayer.Backpack)
-HopperBin.Name = 'Teleport Tool'
-bin = HopperBin
-
-function teleportPlayer(pos)
-local player = game.Players.LocalPlayer
-if player == nil or player.Character == nil then return end
-player.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(pos.x, pos.y + 7, pos.z))
-end
-enabled = true
-function onButton1Down(mouse)
-if not enabled then
-return
-end
-
-local player = game.Players.LocalPlayer
-if player == nil then return end
-
-enabled = false
-local cf = mouse.Hit
-local v = cf.lookVector
-
-teleportPlayer(cf.p)
-
-wait()
-enabled = true
-
-end
-
-function onSelected(mouse)
-mouse.Icon = "rbxasset://textures\\ArrowCursor.png"
-mouse.Button1Down:connect(function() onButton1Down(mouse) end)
-end
-
-bin.Selected:connect(onSelected)
+mouse = game.Players.LocalPlayer:GetMouse()
+tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "Click Tp"
+tool.Activated:connect(function()
+local pos = mouse.Hit+Vector3.new(0,2.5,0)
+pos = CFrame.new(pos.X,pos.Y,pos.Z)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
+end)
+tool.Parent = game.Players.LocalPlayer.Backpack
