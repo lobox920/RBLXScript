e8842c61913050cbe102785682b96a7f6828ffd5
lobox920 <86623018+lobox920@users.noreply.github.com>
2021-09-22 14:47:17 +0000

    Update Secour de france.lua

--- Secour de france.lua
+++ Secour de france.lua
@@ -1,6 +1,6 @@
 ------------------ Desoler au staff etc mais je m'ennuyer et une partie des script vien pas de moi 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Secours de france", "Serpent")
+local Window = Library.CreateLib("Secours de france @lobox920", "Serpent")
 
 ----- Teleportation -----
 local Tab = Window:NewTab("Teleprtation")
